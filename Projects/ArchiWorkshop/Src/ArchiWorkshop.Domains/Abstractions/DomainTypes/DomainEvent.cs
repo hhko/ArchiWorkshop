f70c8fe7bae47d4923e99550d77a1730912b20a0
hhko <hyungho.ko@gmail.com>
2024-01-24 23:56:41 +0900

    DomainEvent 구현 추가

---
+++ Projects/ArchiWorkshop/Src/ArchiWorkshop.Domains/Abstractions/DomainTypes/DomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiWorkshop.Domains.Abstractions.DomainTypes
+{
+    internal class DomainEvent
+    {
+    }
+}
